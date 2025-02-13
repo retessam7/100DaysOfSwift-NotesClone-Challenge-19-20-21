7dd23ff6deb00abaa4efc29e7a97f5891cae1400
retessam7 <retessam7@gmail.com>
2025-02-13 02:33:48 +0100

    updt insets oftext view when the keyboard is shown or hidden

--- Project192021/DetailViewController.swift
+++ Project192021/DetailViewController.swift
@@ -20,11 +20,20 @@ class DetailViewController: UIViewController {
             UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote)),
             UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareNote)),
         ]
+        
+        // ask to be told when the keyboard state changes by using a new class
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        noteText.becomeFirstResponder() // Открывает клавиатуру сразу
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -55,6 +64,25 @@ class DetailViewController: UIViewController {
         let vc = UIActivityViewController(activityItems: [note.text], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view)
+        
+        if notification.name == UIResponder.keyboardWillShowNotification {
+            noteText.contentInset = .zero
+        } else {
+            noteText.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        noteText.scrollIndicatorInsets = noteText.contentInset
+        
+        let selectedRange = noteText.selectedRange
+        noteText.scrollRangeToVisible(selectedRange)
+        
     }
 }
 
